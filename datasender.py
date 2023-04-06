5187f7288959a9a5383867bbf3d7b3a119986953
Trisimm <86274165+Trisimm@users.noreply.github.com>
2023-04-06 17:48:11 -0500

    Add files via upload

--- datasender.py
+++ datasender.py
@@ -4,9 +4,10 @@
 DB_HOST = "softwarec.cwmrd99bpx4t.us-east-1.rds.amazonaws.com"
 DB_NAME = "Group2"
 DB_user = "postgres"
-DB_Pass = "EnterPassword"
+DB_Pass = "Computer"
 
 import psycopg2
+from psycopg2.errors import UniqueViolation
 
 conn = psycopg2.connect(dbname = DB_NAME, user = DB_user, password = DB_Pass, host = DB_HOST)
 
@@ -17,11 +18,19 @@ cur = conn.cursor()
 #    id SERIAL PRIMARY KEY,
 #    FirstName VARCHAR(16) NOT NULL,
 #    LastName VARCHAR(16) NOT NULL,
-#    SpecialNumber BIGINT
+#    SpecialNumber BIGINT UNIQUE
 #);
 #""")
-cur.execute("INSERT INTO ClientData (FirstName, LastName, SpecialNumber) VALUES (%s, %s, %s)", ("John", "Doe", 777))
-
+#cur.execute("INSERT INTO ClientData (FirstName, LastName, SpecialNumber) VALUES (%s, %s, %s)", ("John", "Doe", 777))
+try:
+    # Insert the row into the ClientData table
+    cur.execute("INSERT INTO ClientData (FirstName, LastName, SpecialNumber) VALUES (%s, %s, %s)", ("John", "Doe", 777))
+    # Commit the changes
+    conn.commit()
+except UniqueViolation as e:
+    # Handle the duplicate SpecialNumber error
+    print("Error: A record with the same SpecialNumber already exists.")
+    conn.rollback()
 conn.commit()
 
 cur.close()
