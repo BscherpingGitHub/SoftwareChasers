2e054b8e84021f04081908656b52c85ee8ba90f4
Trisimm <86274165+Trisimm@users.noreply.github.com>
2023-04-16 17:02:19 -0500

    Add files via upload

--- datasender.py
+++ datasender.py
@@ -4,14 +4,14 @@
 DB_HOST = "softwarec.cwmrd99bpx4t.us-east-1.rds.amazonaws.com"
 DB_NAME = "Group2"
 DB_user = "postgres"
-DB_Pass = "null"
+DB_Pass = "Computer"
 
 import psycopg2
-from psycopg2.errors import UniqueViolation
 
 conn = psycopg2.connect(dbname = DB_NAME, user = DB_user, password = DB_Pass, host = DB_HOST)
 
 cur = conn.cursor()
+
 def check_number(fname, Lname, number):
     try:
         # Insert the row into the ClientData table
@@ -22,34 +22,11 @@ def check_number(fname, Lname, number):
     except UniqueViolation as e:
         # Handle the duplicate SpecialNumber error
         print("Error: A record with the same SpecialNumber already exists.")
+        return -1
         conn.rollback()
     conn.commit()
 
     cur.close()
 
     conn.close()
-#cur.execute("""
-#CREATE TABLE ClientData (
-#    id SERIAL PRIMARY KEY,
-#    FirstName VARCHAR(16) NOT NULL,
-#    LastName VARCHAR(16) NOT NULL,
-#    SpecialNumber BIGINT UNIQUE
-#);
-#""")
-#cur.execute("INSERT INTO ClientData (FirstName, LastName, SpecialNumber) VALUES (%s, %s, %s)", ("John", "Doe", 777))
-#try:
-    # Insert the row into the ClientData table
- #   cur.execute("INSERT INTO ClientData (FirstName, LastName, SpecialNumber) VALUES (%s, %s, %s)", ("John", "Doe", 777))
-    # Commit the changes
- #   conn.commit()
-#except UniqueViolation as e:
-    # Handle the duplicate SpecialNumber error
- #   print("Error: A record with the same SpecialNumber already exists.")
-  #  conn.rollback()
-#conn.commit()
-
-#cur.close()
-
-#conn.close()
-
-
+#check_number("hans","doe",1526)
